7aaeea94b0340bdfc4c1676c29488e5bc3fa83aa
khanadat <dattekoda@gmail.com>
2025-05-20 20:45:55 +0900

    del strjoin in libft.h

--- main/includes/libft.h
+++ main/includes/libft.h
@@ -23,6 +23,5 @@ size_t	ft_strlen(const char *s);
 char	*ft_strdup(const char *s);
 size_t	ft_strlcpy(char *dst, const char *src, size_t size);
 char	*ft_strchr(const char *s, int c);
-char	*ft_strjoin(const char *s1, const char *s2);
 
 #endif
